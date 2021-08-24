11ea913ef98dcf638c554dee35568c4182b0bcf8
skirrrr <valento2009@gmail.com>
2021-08-24 16:44:50 -0400

    invisible character :o

--- js/title.js
+++ js/title.js
@@ -1,3 +1,3 @@
-var i=0;setInterval(function(){var titles=["v","va","val","val ","val <","val <3","val <","val ","val","va","v"," ",]
+var i=0;setInterval(function(){var titles=["v","va","val","val ","val <","val <3","val <","val ","val","va","v","ㅤ",]
 if(i===titles.length){i=0;}
 document.title=titles[i];i++;},900);
