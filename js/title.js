074373b07f272c53f610c824071a5501fb08fdfa
skirrrr <valento2009@gmail.com>
2021-08-24 16:59:35 -0400

    new invis character

--- js/title.js
+++ js/title.js
@@ -1,3 +1,3 @@
-var i=0;setInterval(function(){var titles=["v","va","val","val ","val <","val <3","val <","val ","val","va","v","ㅤ",]
+var i=0;setInterval(function(){var titles=["v","va","val","val ","val <","val <3","val <","val ","val","va","v","   ",]
 if(i===titles.length){i=0;}
 document.title=titles[i];i++;},900);
