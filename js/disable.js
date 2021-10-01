61043395eb523d3c38f6a683ac4a8532d5c5d858
skirrrr <valento2009@gmail.com>
2021-10-01 13:38:50 -0400

    disable inspect and right click

---
+++ js/disable.js
@@ -0,0 +1,28 @@
+const body = document.getElementsByTagName('body');
+window.addEventListener("keydown", (event) => {
+  if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
+     event.preventDefault();
+     body[0].innerHTML = "😐"
+  }
+
+  if(event.ctrlKey && (event.key === "C")) {
+     event.preventDefault();
+     body[0].innerHTML = "😐"
+  }
+  if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
+     event.preventDefault();
+     body[0].innerHTML = "😐"
+  }
+  if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
+     event.preventDefault();
+     body[0].innerHTML = "😐";
+  }
+  if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
+     event.preventDefault();
+     body[0].innerHTML = "😐";
+  }
+  if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
+     event.preventDefault();
+     body[0].innerHTML = "😐";
+  }
+});
