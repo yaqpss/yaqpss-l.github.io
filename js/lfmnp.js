f580fb6bcbc948edd0c6f993e3a47c9706091ad1
skirrrr <valento2009@gmail.com>
2021-10-11 17:22:55 -0400

    testing artist redirect

--- js/lfmnp.js
+++ js/lfmnp.js
@@ -30,6 +30,7 @@ var getSetLastFM = function() {
       var artistFormatted =
         recentTrack.artist["#text"];
       $("a#trackartist")
+        .attr("href", "https://last.fm/music/" + recentTrack.artist["#text"])
         .html(artistFormatted)
         .attr("title", "Artist : " + recentTrack.artist["#text"]);
       $("img#trackart").attr("src", recentTrack.image[2]["#text"]);
